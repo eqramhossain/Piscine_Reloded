5216f517386f6d379353fb1fd28eaec4f9d689fb
ehossain <ekram.fr03@gmail.com>
2025-01-02 22:09:11 +0100

    ft_iterative not working

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -1,39 +1,22 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ehossain <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/05 17:56:38 by ehossain          #+#    #+#             */
-/*   Updated: 2024/12/25 18:03:09 by ehossain         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 
-// int	ft_iterative_factorial(int nb);
-//
-// int	main(void)
-// {
-// 	int	i;
-//
-// 	i = ft_iterative_factorial(12);
-// 	printf("%d", i);
-// 	return (0);
-// }
+int	ft_iterative_factorial(int nb);
+
+int	main(void)
+{
+	printf("%d\n", ft_iterative_factorial(5));
+	return (0);
+}
 
 int	ft_iterative_factorial(int nb)
 {
-	int	result;
+	int	res;
 
-	result = 1;
-	if (nb <= 0 || nb > 12)
-		return (0);
-	while (nb > 0)
+	res = nb;
+	while (nb > 1)
 	{
-		result = result * nb;
+		res = res * nb;
 		nb--;
 	}
-	return (result);
+	return (res);
 }
